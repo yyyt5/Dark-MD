d0dcf0771aa1f88e761246c5f78627acce2bf33f
sudu25-hash <randulayasiru2008@gmail.com>
2025-03-15 09:41:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~yyBUVK6J#1ZUrVkqsKiU0CB-re__KwKzJDMFxsjRQs5ZGm_lnNUs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
